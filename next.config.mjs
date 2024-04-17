8a588fbe3c9d09f3a449841953c26ce000dd0152
FSTechno <88673222+MIRX562@users.noreply.github.com>
2024-04-17 09:44:35 +0700

    image config added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			//github profile image
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com',
+			},
+			//google profile image
+			{
+				protocol: 'https',
+				hostname:
+					'https://lh3.googleusercontent.com/a/ACg8ocITzFvjoF6uxY_p8zeEShwA7JTt1miLkmbn729n_P8dQvbn7w=s96-c',
+			},
+		],
+	},
+};
 
 export default nextConfig;
