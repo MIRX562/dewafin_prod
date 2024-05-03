a659c98fd0a8d66af3c59ac6f282228b2e67269e
FSTechno <88673222+MIRX562@users.noreply.github.com>
2024-05-03 09:53:03 +0700

    icon opt

--- src/components/authComponents/errorCard/ErrorCard.tsx
+++ src/components/authComponents/errorCard/ErrorCard.tsx
@@ -1,17 +1,19 @@
 import { Card, CardFooter, CardHeader } from "@/components/ui/card";
-import React from "react";
 import Header from "../header/Header";
 import BackButton from "../loginBackButton/BackButton";
 
 export default function ErrorCard() {
-  return (
-    <Card className="w-[400px] shadow-md">
-      <CardHeader>
-        <Header label="Oops! Something went wrong" />
-      </CardHeader>
-      <CardFooter>
-        <BackButton label="Back to Login" href="/auth/login" />
-      </CardFooter>
-    </Card>
-  );
+	return (
+		<Card className="w-[400px] shadow-md">
+			<CardHeader>
+				<Header label="Oops! Something went wrong" />
+			</CardHeader>
+			<CardFooter>
+				<BackButton
+					label="Back to Login"
+					href="/auth/login"
+				/>
+			</CardFooter>
+		</Card>
+	);
 }
