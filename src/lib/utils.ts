af8559fe1fd884aed2c4385a20750bf64e1c9942
FSTechno <88673222+MIRX562@users.noreply.github.com>
2024-05-03 17:07:22 +0700

    fixesss

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,20 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+	return twMerge(clsx(inputs));
+}
+
+export function formatFileSize(bytes: number) {
+	if (typeof bytes !== "number" || bytes < 0) return "Invalid file size";
+
+	const units = ["B", "KB", "MB", "GB", "TB"];
+	let threshold = 1024;
+	let i = 0;
+
+	while (bytes >= threshold && i < units.length - 1) {
+		bytes /= threshold;
+		i++;
+	}
+
+	return `${bytes.toFixed(1)} ${units[i]}`;
 }
