ff02910063709c281915bf492961c73372bf11bf
FSTechno <88673222+MIRX562@users.noreply.github.com>
2024-05-08 20:51:04 +0700

    after PWA

--- next.config.mjs
+++ next.config.mjs
@@ -21,4 +21,20 @@ const nextConfig = {
 	},
 };
 
-export default nextConfig;
+// export default nextConfig;
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+	dest: "public",
+	cacheOnFrontEndNav: true,
+	aggressiveFrontEndNavCaching: true,
+	reloadOnOnline: true,
+	swcMinify: true,
+	disable: false,
+	workboxOptions: {
+		disableDevLogs: true,
+	},
+});
+
+export default withPWA(nextConfig);
