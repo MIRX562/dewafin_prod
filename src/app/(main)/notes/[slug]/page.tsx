5747102a0a25335a5a7e45ce6f632a1543db6ee0
FSTechno <88673222+MIRX562@users.noreply.github.com>
2024-05-11 11:19:19 +0700

    l ll

---
+++ src/app/(main)/notes/[slug]/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import NoteEditor from "@/components/NoteEditor/NoteEditor";
+
+export default function NoteView() {
+	return <NoteEditor />;
+}
---
+++ src/app/(main)/notes/new-note/page.tsx
@@ -0,0 +1,38 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/RvS98os53Yq
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ */
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SaveIcon } from "lucide-react";
+import NoteView from "../[slug]/page";
+
+export default function NewNote() {
+	return (
+		<div className="flex h-full w-full">
+			<div className="flex flex-1 flex-col">
+				<header className="flex h-16 items-center border-b ">
+					<div className="flex w-full items-center justify-between gap-2">
+						<Input
+							className="w-full rounded-md border-none text-3xl focus:border-l "
+							placeholder="Note title"
+							type="text"
+						/>
+						<Button
+							variant="outline"
+							className="gap-2"
+						>
+							<SaveIcon className="h-5 w-5" />
+							Save Note
+						</Button>
+					</div>
+				</header>
+				<div className="flex-1 overflow-auto">
+					<NoteView />
+				</div>
+			</div>
+		</div>
+	);
+}
